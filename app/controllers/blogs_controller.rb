ffa5d8ee22d1fdddb62ebf4306c0ec07bdbe06e2
yossy-infratop <yoshikawa.akito.infratop@gmail.com>
2021-10-30 06:27:18 +0000

    [finish]chapter8

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,9 +12,12 @@ class BlogsController < ApplicationController
   end
 
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blog_path(blog.id)
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(blog.id)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -25,6 +28,12 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
 
   private
